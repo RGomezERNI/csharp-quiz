100a33fa1178ad0b3963a34b699a3865c54cc048
Gomez Rafael (External) <rafael.gomez@betterask.erni>
2025-02-03 11:41:36 +0800

    removed comments.

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -7,18 +7,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        /***
-        var serviceProvider = new ServiceCollection()
-                .AddLogging(configure => {
-                    configure.AddConsole(options => options.LogToStandardErrorThreshold = LogLevel.Warning);
-                    configure.SetMinimumLevel(LogLevel.Warning);
-                    })
-                .BuildServiceProvider();
 
-        var logger = serviceProvider.GetService<ILogger<Program>>();
-
-        logger.LogInformation("Calculator application started.");
-        ***/
         using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
         ILogger logger = factory.CreateLogger("Program");
         logger.LogInformation("Welcome to My Calculator App");
@@ -31,20 +20,16 @@ class Program
             Console.WriteLine("Enter the first number:");
             string input1 = Console.ReadLine();
             var num1 = calculator.ParseInput(input1);
-            //logger.LogInformation($"User entered first number: {num1}");
 
             Console.WriteLine("Enter the second number:");
             string input2 = Console.ReadLine();
             var num2 = calculator.ParseInput(input2);
-            //logger.LogInformation($"User entered second number: {num2}");
 
             Console.WriteLine("Enter the operation (add, subtract, multiply, divide):");
             string operation = Console.ReadLine()?.ToLower() ?? string.Empty;
-            //logger.LogInformation($"User selected operation: {operation}");
 
             double result = calculator.PerformOperation(num1, num2, operation);
             Console.WriteLine($"The result is: {result}");
-            //logger.LogInformation($"Calculation performed: {operation} {num1} and {num2}. \n Result is equal to : {result}");
 
             Console.WriteLine("Calculation attempt finished.");
             logger.LogInformation("Calculation Attempt Successful");
